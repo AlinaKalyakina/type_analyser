ffc1bf6039f1446f60883a33e209ac13f96a2ec6
Alina Kalyakina <s02160070@stud.cs.msu.ru>
2018-04-17 21:00:14 +0300

    More tests were added

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -7,8 +7,9 @@ VAL_RES=./valgrind_output
 mkdir -p  $RES
 rm -f $RES/$TEST_RES
 rm -f $RES/$VAL_RES
-for VAR in " \n \t" "i+j+k" "1+j*ai[6548]" "f"  "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai"
+for VAR in " \n \t\n" "i+j+k" "1+j*ai[6548]" "f"  "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai"
 do
+echo $VAR >> $RES/$TEST_RES
 echo -n -e $VAR >> $RES/$TEST_RES
 echo -n -e $VAR > tmp
 echo $VAR;
