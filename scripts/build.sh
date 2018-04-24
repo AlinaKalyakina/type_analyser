0597d08851c2ea00914a60797b3caba305d14b75
Alina Kalyakina <s02160070@stud.cs.msu.ru>
2018-04-24 21:43:35 +0300

    Testing was made automatic. Error in build script was fixed

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,11 @@
 SRC=./src/
 INC=./inc/
 EXE=./exe/
+mkdir -p $EXE
 cd $EXE
 for VAR in analysistree errors lexseq main synanalys typedetector testmain
 do
-g++-7 --coverage -Wall -c -std=gnu++1z ../$SRC/$VAR.cpp -o ./$VAR.o -I ../$INC
+g++-7 --coverage -Wall -O3 -fno-omit-frame-pointer -fno-optimize-sibling-calls -c -std=gnu++1z ../$SRC/$VAR.cpp -o ./$VAR.o -I ../$INC
 done
 g++-7 --coverage -Wall -std=gnu++1z analysistree.o errors.o lexseq.o main.o synanalys.o typedetector.o -o analyser.out -I ../$INC
 g++-7 --coverage -Wall -std=gnu++1z analysistree.o errors.o lexseq.o testmain.o synanalys.o typedetector.o -o testmain.out -I ../$INC
--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,21 +2,19 @@
 OUT=./analyser.out
 EXE=./exe/
 RES=./results/
-TEST_RES=./test_result
+TESTS=./tests/
+RIGHT=./right_ans/
 VAL_RES=./valgrind_output
-mkdir -p  $RES
-rm -f $RES/$TEST_RES
+mkdir -p $RES
+mkdir -p $TESTS
 rm -f $RES/$VAL_RES
-for VAR in " \n \t\n" "i+j+k" "1+j*ai[65555555555555555555555555555555555555555555555555555555555555548]" "f"  "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai"
+for VAR in " " "\n \t\n" "i+j+k" "1+j*ai[65555555555555555555555555555555555555555555555555555555555555548]" "f"  "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai" "laast"
 do
-echo $VAR >> $RES/$TEST_RES
-echo -n -e $VAR >> $RES/$TEST_RES
-echo -n -e $VAR > tmp
-echo $VAR;
-valgrind --quiet --leak-check=full $EXE/$OUT<tmp 1>>$RES/$TEST_RES 2>>$RES/$VAL_RES
-echo -e "\n*******" >> $RES/$TEST_RES
+echo $VAR
+echo -n -e $VAR>$TESTS/${VAR// /_}
+valgrind --quiet --leak-check=full $EXE/$OUT<$TESTS/${VAR// /_} 1>$RES/${VAR// /_} 2>>$RES/$VAL_RES
+diff -q -b -Z -B $RES/${VAR// /_} $RIGHT/${VAR// /_}
 done
-echo -n -e "f" > tmp
-$EXE/testmain.out >> $RES/$TEST_RES <tmp
-rm tmp
+echo -n -e "f" >$TESTS/testmain
+$EXE/testmain.out >$RES/testmain  <$TESTS/testmain
 
