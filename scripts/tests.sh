9c3a089d1f2022b4f8a3627bc7471148b24bfb7a
Alina Kalyakina <s02160070@stud.cs.msu.ru>
2018-04-26 08:13:26 +0300

    More tests for expressions in parenthesis were added

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -8,7 +8,7 @@ VAL_RES=./valgrind_output
 mkdir -p $RES
 mkdir -p $TESTS
 rm -f $RES/$VAL_RES
-for VAR in " " "\n \t\n" "i+j+k" "1+j*ai[65555555555555555555555555555555555555555555555555555555555555548]" "f"  "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai" "laast"
+for VAR in "\n \t\n" "(((i)))" "i+(j+k)" "1+j*ai[65555555555555555555555555555555555555555555555555555555555555548]" "f" "(aai[k])[ai[k]]+5" "fst"  "fijkaasai" "i    + \n (k+1)" "\tas\t" "i\n\n+  j" "aai[i][j]*ai[j]+5" "aa+aa" "k+\n\t   faas" "f5as" "faf?" "fa5jk" "i+ i5s98" "5i+1" "6+56[i]+6" "fjfk(" "ff" "ai[j]+at[k]"  "3++5" "4+\nai[i" "(1+2]" "+i" "1+" "3+5)" "5@1" "i+ai[j][k]" "  as[s]+5" "afasasais+5" "5*fai" "laast"
 do
 echo $VAR
 echo -n -e $VAR>$TESTS/${VAR// /_}
